429298b1962cf9a042eba744b911b57ab8befac3
Ксения Финаева <polya.makova@outlook.com>
2021-11-23 12:48:05 +0300

    Добавьте файлы проекта.

---
+++ Task6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6._2
+{
+    class Program
+    {
+        // Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом.
+        // Знаки препинания не используются. Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра
+        // (пример палиндрома – «А роза упала на лапу Азора»).
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string resultString = "";
+            foreach (string s in stringArray)
+            {
+                resultString += s.Substring(0, 1).ToLower() + s.Substring(1) + s.Trim() + " ";
+
+            }
+            Console.WriteLine(resultString);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются.
+            // Найти самое длинное слово в строке.
+            Console.WriteLine("Введите предложение: ");
+            string s = Console.ReadLine();
+            string[] stringArray = s.Split();
+            int max = 0;
+            foreach (string a in stringArray)
+            {
+                int n = Convert.ToInt32(a.Length);
+                if (n > max)
+                    max = n;
+            }
+            Console.WriteLine("Количество знаков в самом длинном слове равно: {0} ", max);
+            Console.ReadKey();
+        }
+    }
+}
